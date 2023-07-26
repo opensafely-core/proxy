99228b8a957f4371a8ed76eec05b4cb0e9aa5f80
Tom Ward <tomward@fmail.co.uk>
2023-07-26 13:10:33 +0100

    chore: blacken the python file

--- check-header.py
+++ check-header.py
@@ -3,11 +3,11 @@ import requests
 
 resp = requests.get("https://ghcr.io/v2/")
 expected = 'Bearer realm="https://ghcr.io/token",service="ghcr.io",scope="repository:user/image:pull"'
-header = resp.headers['www-authenticate']
+header = resp.headers["www-authenticate"]
 
 print("www-authenticate: " + header)
 
 if header == expected:
-    print('www-authenticate is as expected')
+    print("www-authenticate is as expected")
 else:
-    sys.exit('www-authenticate header has changed!')
+    sys.exit("www-authenticate header has changed!")
